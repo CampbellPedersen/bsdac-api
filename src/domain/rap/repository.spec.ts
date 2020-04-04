7b69c72079c9b51a952bb22944b843a11ee9bcb1
Campbell <campbelljip@gmail.com>
2020-04-04 18:44:30 +0800

    Add in memory rap repository

---
+++ src/domain/rap/repository.spec.ts
@@ -0,0 +1,68 @@
+import { RapRepository, inMemoryRapRepository } from './repository';
+
+const theFirstRap = {
+  id: 'rap-001',
+  title: 'The First Rap',
+  artist: 'Campbell Pedersen',
+  appearedAt: { event: 'bsdac', series: 1 },
+};
+
+const theSecondRap = {
+  id: 'rap-002',
+  title: 'The Second Rap',
+  artist: 'Dennis Nguyen',
+  appearedAt: { event: 'bsdac', series: 3 },
+};
+
+const baseTests = (repository: RapRepository) => () => {
+  it('loads undefined when rap does not exist', async () => {
+    await expect(repository.load('1')).resolves.toBeUndefined();
+  });
+
+  it('loads empty array when no raps are saved', async () => {
+    await expect(repository.loadAll())
+      .resolves.toHaveLength(0);
+  });
+
+  it('saves and loads one rap', async () => {
+    await repository.save(theFirstRap);
+
+    await expect(repository.load('rap-001'))
+      .resolves.toStrictEqual(theFirstRap);
+  });
+
+  it('loads an array of one when one rap exists', async () => {
+    await expect(repository.loadAll())
+      .resolves.toStrictEqual([ theFirstRap ]);
+  });
+
+  it('overwrites one rap', async () => {
+    const editedRap = { ...theFirstRap, title: 'Actually, scratch that' };
+    await repository.save(editedRap);
+
+    await expect(repository.load('rap-001'))
+      .resolves.toStrictEqual(editedRap);
+
+    await repository.save(theFirstRap);
+
+    await expect(repository.load('rap-001'))
+      .resolves.toStrictEqual(theFirstRap);
+  });
+
+  it('saves and loads individual raps', async () => {
+    await repository.save(theSecondRap);
+
+    await expect(repository.load('rap-001'))
+      .resolves.toStrictEqual(theFirstRap);
+    await expect(repository.load('rap-002'))
+      .resolves.toStrictEqual(theSecondRap);
+  });
+
+  it('loads all raps', async () => {
+    await expect(repository.loadAll())
+      .resolves.toStrictEqual([ theFirstRap, theSecondRap ]);
+  });
+};
+
+const inMemory = inMemoryRapRepository();
+describe('in-memory-rap-repository', baseTests(inMemory));
---
+++ src/domain/rap/repository.ts
@@ -0,0 +1,33 @@
+interface Appearance {
+  event: string
+  series: number
+}
+
+interface PersistedRap {
+  title: string
+  artist: string
+  appearedAt: Appearance
+}
+
+export type Rap = PersistedRap & { id: string }
+
+export interface RapRepository {
+  loadAll(): Promise<Rap[]>
+  load(id: string): Promise<Rap | undefined>
+  save(rap: Rap): Promise<void>
+}
+
+export const inMemoryRapRepository = (): RapRepository => {
+  const store: { [id: string]: PersistedRap } = {};
+  return {
+    loadAll: async () => Object.keys(store).map(id => ({ id, ...store[id] })),
+    load: async (id: string) => store[id] ? { id, ...store[id]} : undefined,
+    save: async (rap: Rap) => {
+      store[rap.id] = {
+        appearedAt: rap.appearedAt,
+        title: rap.title,
+        artist: rap.artist
+      };
+    },
+  };
+};
