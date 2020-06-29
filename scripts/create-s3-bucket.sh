3cdb19ca12db739f90e7ec33a3a418a8f32254d0
Campbell <campbelljip@gmail.com>
2020-06-29 17:46:44 +0800

    Finalise naming and ports

--- scripts/create-s3-bucket.sh
+++ scripts/create-s3-bucket.sh
@@ -1,2 +1,2 @@
-aws --endpoint-url=http://localhost:4572 s3 mb s3://bsdapp-rap-audio
-aws --endpoint-url=http://localhost:4572 s3api put-bucket-acl --bucket bsdapp-rap-audio --acl public-read
+aws --endpoint-url=http://localhost:4566 s3 mb s3://bsdapp-rap-audio
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket bsdapp-rap-audio --acl public-read
