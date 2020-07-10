74f0b2ef474c399729294bf56d829ca2d3503658
Campbell <campbelljip@gmail.com>
2020-07-11 03:09:42 +0800

    Update loading phrases

--- client/src/components/loading-screen.tsx
+++ client/src/components/loading-screen.tsx
@@ -3,18 +3,17 @@ import './loading-screen.css';
 
 const loadingPhrases = [
   'Putting it in the raps',
-  'Welcoming you to BSDAC 11',
   'Purchasing hand warmers',
-  'Drinking peach iced tea 1.5L',
+  'Placing speaker in the center',
+  'Cracking open a beer',
+  'First-picking armored bee',
 ];
+const randomPhrase = () => loadingPhrases[Math.floor(Math.random() * loadingPhrases.length)];
 
-export const LoadingScreen = () => {
-  const randomPhrase = () => loadingPhrases[Math.floor(Math.random() * loadingPhrases.length)];
-
-  return <div className='loading-screen'>
+export const LoadingScreen = () =>
+  <div className='loading-screen'>
     <div className="spinner-border spinner-border-lg" role="status">
       <span className="sr-only">Loading...</span>
     </div>
     <h4 className='font-weight-light mt-3'>{randomPhrase()}...</h4>
   </div>;
-};
