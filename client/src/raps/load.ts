a18eff68c1e0a9613505ff20dc10a10106dc22d4
Campbell <campbelljip@gmail.com>
2020-07-14 22:36:01 +0800

    Add delay for loading screen so we can read the memes

--- client/src/raps/load.ts
+++ client/src/raps/load.ts
@@ -3,6 +3,11 @@ import { useContext } from 'react';
 import { AppContext } from '../context';
 import { Rap } from './types';
 
+const delay = (
+  cb: () => void,
+  delay: number
+) => new Promise(resolve => setTimeout(() => {cb(); resolve(); }, delay));
+
 export const loadRaps = (
   isLoading: boolean,
   requested: () => void,
@@ -15,8 +20,7 @@ export const loadRaps = (
 
     requested();
     const raps = await requestRaps();
-    received(raps);
-    // received(JSON.parse('[{"bonus":true,"imageUrl":"https://i.imgur.com/Q7M3sh2.png","id":"0ba75e4d-080d-43ee-a615-c5769d7afc78","title":"A bonus track","lyrics":"Some other words mun","appearedAt":{"name":"BSDAC","series":11},"rapper":"Campbell Pedersen"},{"bonus":false,"imageUrl":"https://i.imgur.com/qJjv1Ba.jpg","id":"8c0ffd38-0c36-4e7d-bee4-903050ad43b4","title":"Another rap","lyrics":"Some other words mun","appearedAt":{"name":"BSDAC","series":11},"rapper":"Nathan Kosc"},{"bonus":false,"imageUrl":"https://i.imgur.com/AQRiRER.jpg","id":"11fb4ed4-d16a-4794-8770-42e5ec82af2c","title":"BigGreen\'s Back","lyrics":"Words mun","appearedAt":{"name":"BSDAC","series":12},"rapper":"Campbell Pedersen"}]'));
+    await delay(() => received(raps), 1000);
   };
 };
 
