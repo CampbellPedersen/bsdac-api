c43cd5567e69bd402d30769ffb9d28f2f2eb91ee
Campbell <campbelljip@gmail.com>
2021-03-20 10:17:05 +1100

    Add BEATS 2 to dropdown

--- client/src/raps/constants.ts
+++ client/src/raps/constants.ts
@@ -33,4 +33,5 @@ export const events: Event[] = [
   { name: EventName.BSDAC, series: 12 },
   { name: EventName.BSDAC, series: 13 },
   { name: EventName.BEATS, series: 1 },
+  { name: EventName.BEATS, series: 2 },
 ];
